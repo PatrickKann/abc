2dae5a1fdca4f3fa5579e10228cca9a11f708f9f
PatrickKann <52541434+PatrickKann@users.noreply.github.com>
2019-07-18 11:35:58 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,6 @@
+import { ClassInfo } from "../../app/classInfo";
+
+export const MYCLASSES: ClassInfo[] = [
+  {  cname : 'SYST24444', room : 'G202', dow : 'Thurs', tod : '9-11' },
+  {  cname : 'SYST35300', room : 'B110', dow : 'Fri', tod : '12-15' }
+]
